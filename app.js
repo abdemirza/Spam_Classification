2878da7fdf12feab4b8ff56498358b5d82998be4
abdemirza <abdemirza@gmail.com>
2020-07-28 15:25:05 +0530

    created the user interface for the app and then predicting the value

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const bodyParser = require("body-parser");
+const spawn = require("child_process").spawn;
+// To get post params
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/", function (req, res) {
-  res.render('index.ejs')
+  res.render("index.ejs",{'output':-1});
 });
+app.post("/", function (req, res) {
+  var process = spawn("python", ["bayesModel.py", req.body.email]);
 
+  process.stdout.on("data", function (data) {
+     res.render('index.ejs',{'output':data.toString()})
+  });
+});
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
